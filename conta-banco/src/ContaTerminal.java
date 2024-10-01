bf89a3b218c4a0ba2caa7cb9fb817c233b2fb8e7
luisp <luisfelipe_pellis@hotmail.com>
2024-10-01 11:12:57 -0300

    Desafio proposto pelo Bootcamp da Dio, criar uma conta banco para reforçar os conhecimentos com a classe Scanner, Terminal e exibir resultado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        // TODO: Importando a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao Banco para continuar insira as informações a seguir: ");
+
+        // Exibindo a mensagem para o usuário 
+        System.out.println("Digite seu nome:  ");
+        // Coletando a informação inserida pelo usuário 
+        String nomecliente = scanner.next();
+       
+        System.out.println("Digite o numero da sua conta:  ");
+        int numeroconta = scanner.nextInt();
         
-        // TODO: Declarando as variáveis 
+        System.out.println("Digite sua agencia: ");
+        String agencia = scanner.next();
 
-        String userName;
-        double numeroConta;
-        double agenciaConta;
-        double saldoConta;
+        System.out.println("Digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
 
-        
-
-
-
-
+        // Exibindo a mensagem de conta criada com sucesso !!
+        System.out.println("Olá " + nomecliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroconta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
